3ee4d757d841e85db1fdf1cc65f2933d482438f2
Gustavo Porto <gporto@quanticosolutions.com>
2020-10-08 15:50:47 +0100

    :white_check_mark: update vow tests to use jest-in-case module

--- src/__tests__/vow.test.js
+++ src/__tests__/vow.test.js
@@ -1,3 +1,5 @@
+import cases from 'jest-in-case';
+
 import { Vow } from '../vow';
 
 describe('Vow', () => {
@@ -5,5 +7,20 @@ describe('Vow', () => {
 		test('throw an error if the executor is missing', () => {
 			expect(() => new Vow()).toThrowError(/not a function/);
 		});
+
+		cases(
+			'throw an error if the executor is not a function',
+			opts => {
+				expect(() => new Vow(opts.executor)).toThrowError(
+					/must be a function/
+				);
+			},
+			[
+				{ name: 'String', executor: 'random' },
+				{ name: 'Number', executor: 1 },
+				{ name: 'Array', executor: [] },
+				{ name: 'Object', executor: {} },
+			]
+		);
 	});
 });
